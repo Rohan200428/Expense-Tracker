a45f4614111602628630289c07fc03130a706842
Rohan200428 <svsrrohan@gmail.com>
2025-10-13 15:07:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,4 +32,4 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`AWS Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`AWS server running on port ${PORT}`));
